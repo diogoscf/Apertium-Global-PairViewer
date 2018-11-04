28acbcaa5899cb68433fcca63aaf7655e71b3dc2
dolphingarlic <dolphingarlic@gmail.com>
2018-11-04 21:33:14 +0200

    Made some stylistic changes

--- trimpairs.py
+++ trimpairs.py
@@ -6,15 +6,15 @@ languages with coordinates in the tsv file
 import json
 
 
-def filterPairs(filename, languages, altCodes):
+def filter_pairs(filename, languages, alt_codes):
     """
     Filters out any language that is not in
     the list of valid languages
     """
 
-    pairFile = open(filename, "r")
+    pair_file = open(filename, "r")
     filtered = []
-    for line in pairFile:
+    for line in pair_file:
         line = line.strip().split()
 
         lang1 = line[3][1:-2]
@@ -27,87 +27,87 @@ def filterPairs(filename, languages, altCodes):
             filtered.append(joined)
 
         elif (
-                lang1 in altCodes and altCodes[lang1] in languages
+                lang1 in alt_codes and alt_codes[lang1] in languages
         ) and (
-                lang2 in altCodes and altCodes[lang2] in languages
+            lang2 in alt_codes and alt_codes[lang2] in languages
         ):
             # Getting rid of last comma
             line[-1] = line[-1][:-1]
-            line[1] = line[1][0] + altCodes[lang2] + line[1][-2:]
-            line[3] = line[3][0] + altCodes[lang1] + line[3][-2:]
+            line[1] = line[1][0] + alt_codes[lang2] + line[1][-2:]
+            line[3] = line[3][0] + alt_codes[lang1] + line[3][-2:]
             joined = " ".join(line)
             filtered.append(joined)
 
-    pairFile.close()
+    pair_file.close()
     return filtered
 
 
-def getLanguagesWithCoords(filename):
+def get_languages_with_coords(filename):
     """
     Gets the languages with their coordinates
     on the Earth
     """
 
-    langFile = open(filename, "r")
-    langDict = {}
-    for line in langFile:
+    lang_file = open(filename, "r")
+    lang_dict = {}
+    for line in lang_file:
         line = line.strip().split(",")
         lat, lon = float(line[1]), float(line[2])
-        langDict[line[0]] = [lon, lat]
-    langFile.close()
-    return langDict
+        lang_dict[line[0]] = [lon, lat]
+    lang_file.close()
+    return lang_dict
 
 
-def getAlternateCodes(filename):
+def get_alternate_codes(filename):
     """
     Reads the alternate codes for languages from file
     and then returns a dict of that data
     """
 
-    codeFile = open(filename, 'r')
-    codeStr = codeFile.read()
-    codeFile.close()
-    return json.loads(codeStr)
+    code_file = open(filename, 'r')
+    code_str = code_file.read()
+    code_file.close()
+    return json.loads(code_str)
 
 if __name__ == "__main__":
-    langDict = getLanguagesWithCoords("apertium-languages.tsv")
-    altCodes = getAlternateCodes('codes.json')
-    filtered = filterPairs("pairs.json.txt", langDict, altCodes)
-    apertiumFile = open("apertiumPairs.json", "w")
+    LANG_DICT = get_languages_with_coords("apertium-languages.tsv")
+    ALT_CODES = get_alternate_codes('codes.json')
+    FILTERED = filter_pairs("pairs.json.txt", LANG_DICT, ALT_CODES)
+    APERTIUM_FILE = open("apertiumPairs.json", "w")
 
-    apertiumFile.write("{\n")
-    apertiumFile.write('"type": "FeatureCollection",\n')
-    apertiumFile.write('"pairs": [\n')
-    for line in filtered[:-1]:
-        apertiumFile.write("\t"+line+"\n")
-        apertiumFile.write(",\n")
+    APERTIUM_FILE.write("{\n")
+    APERTIUM_FILE.write('"type": "FeatureCollection",\n')
+    APERTIUM_FILE.write('"pairs": [\n')
+    for line in FILTERED[:-1]:
+        APERTIUM_FILE.write("\t"+line+"\n")
+        APERTIUM_FILE.write(",\n")
 
-    apertiumFile.write("\t"+filtered[-1]+"\n")
+    APERTIUM_FILE.write("\t"+FILTERED[-1]+"\n")
 
-    apertiumFile.write("]\n")
-    apertiumFile.write("}")
+    APERTIUM_FILE.write("]\n")
+    APERTIUM_FILE.write("}")
 
-    apertiumFile.close()
+    APERTIUM_FILE.close()
 
-    apertiumFile2 = open("apertiumPoints.json", "w")
-    apertiumFile2.write("{\n")
-    apertiumFile2.write('"type": "FeatureCollection",\n')
+    APERTIUM_FILE2 = open("apertiumPoints.json", "w")
+    APERTIUM_FILE2.write("{\n")
+    APERTIUM_FILE2.write('"type": "FeatureCollection",\n')
 
     # Writing point coordinates
-    apertiumFile2.write('"point_data": [\n')
-    langArr = []
-    for code in sorted(langDict.keys()):
-        langArr.append([code, langDict[code]])
+    APERTIUM_FILE2.write('"point_data": [\n')
+    LANG_ARR = []
+    for code in sorted(LANG_DICT.keys()):
+        LANG_ARR.append([code, LANG_DICT[code]])
 
-    for lang in langArr[:-1]:
-        string = '{"type": "Feature", "tag": "' + lang[0] + '", ' + '"geometry": { "type": "Point", ' + '"coordinates": ' + str(lang[1]) + "} }\n"
-        apertiumFile2.write("\t"+string)
-        apertiumFile2.write(",\n")
+    for lang in LANG_ARR[:-1]:
+        STRING = '{"type": "Feature", "tag": "' + lang[0] + '", ' + '"geometry": { "type": "Point", ' + '"coordinates": ' + str(lang[1]) + "} }\n"
+        APERTIUM_FILE2.write("\t" + STRING)
+        APERTIUM_FILE2.write(",\n")
 
-    string = '{"type": "Feature", "tag": "' + langArr[-1][0] + '", ' + '"geometry": { "type": "Point", ' + '"coordinates": ' + str(langArr[-1][1]) + "} }\n"
-    apertiumFile2.write("\t"+string)
+    STRING = '{"type": "Feature", "tag": "' + LANG_ARR[-1][0] + '", ' + '"geometry": { "type": "Point", ' + '"coordinates": ' + str(LANG_ARR[-1][1]) + "} }\n"
+    APERTIUM_FILE2.write("\t" + STRING)
 
-    apertiumFile2.write("]\n")
-    apertiumFile2.write("}")
+    APERTIUM_FILE2.write("]\n")
+    APERTIUM_FILE2.write("}")
 
-    apertiumFile2.close()
+    APERTIUM_FILE2.close()
