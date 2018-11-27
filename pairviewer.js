36f5676dd5f72847d11ba983d4a425c0581fb687
dolphingarlic <dolphingarlic@gmail.com>
2018-11-27 10:10:44 +0200

    Formatted showModal with Prettier

--- pairviewer.js
+++ pairviewer.js
@@ -205,7 +205,6 @@ function getPairs(d) {
       let data = [links[i].targetTag, links[i].stems, links[i].stage];
 
       pairs.push(data);
-
     } else if (links[i].targetTag === d) {
       let data = [links[i].sourceTag, links[i].stems, links[i].stage];
 
@@ -267,63 +266,90 @@ function displayModal(d, pairs) {
     .attr("width", width)
     .attr("height", height);
 
-  let nodes = [{id: d, label: codeToLanguage(d), color: "#2196F3"}];
+  let nodes = [{ id: d, label: codeToLanguage(d), color: "#2196F3" }];
   let connections = [];
   for (i = 0; i < pairs.length; i++) {
-    nodes.push({id: pairs[i][0], label: codeToLanguage(pairs[i][0]), color: chooseNodeColor(pairs[i][2])});
-    connections.push({source: d, target: pairs[i][0], stems: pairs[i][1], color: chooseNodeColor(pairs[i][2]), strength: 0.05});
+    nodes.push({
+      id: pairs[i][0],
+      label: codeToLanguage(pairs[i][0]),
+      color: chooseNodeColor(pairs[i][2])
+    });
+    connections.push({
+      source: d,
+      target: pairs[i][0],
+      stems: pairs[i][1],
+      color: chooseNodeColor(pairs[i][2]),
+      strength: 0.05
+    });
   }
 
   const linkForce = d3
     .forceLink()
-    .id(function (link) { return link.id })
-    .strength(function (link) { return link.strength });
+    .id(function(link) {
+      return link.id;
+    })
+    .strength(function(link) {
+      return link.strength;
+    });
 
-  const simulation = d3.forceSimulation()
+  const simulation = d3
+    .forceSimulation()
     .force("link", linkForce)
     .force("charge", d3.forceManyBody().strength(-100))
     .force("center", d3.forceCenter(width / 2, height / 2));
 
-  const dragDrop = d3.drag().on("start", function (node) {
-      node.fx = node.x
-      node.fy = node.y
-    }).on("drag", function (node) {
-      simulation.alphaTarget(0.7).restart()
-      node.fx = d3.event.x
-      node.fy = d3.event.y
-    }).on("end", function (node) {
+  const dragDrop = d3
+    .drag()
+    .on("start", function(node) {
+      node.fx = node.x;
+      node.fy = node.y;
+    })
+    .on("drag", function(node) {
+      simulation.alphaTarget(0.7).restart();
+      node.fx = d3.event.x;
+      node.fy = d3.event.y;
+    })
+    .on("end", function(node) {
       if (!d3.event.active) {
-        simulation.alphaTarget(0)
+        simulation.alphaTarget(0);
       }
-      node.fx = null
-      node.fy = null
+      node.fx = null;
+      node.fy = null;
     });
 
-  const linkElements = svgContainer.append("g")
+  const linkElements = svgContainer
+    .append("g")
     .selectAll("line")
     .data(connections)
-    .enter().append("line")
-      .attr("stroke-width", d => d.stems < 0 ? 2 : Math.log(d.stems / 100, 2) * 3 + 4)
-      .attr("stroke", d => d.stems < 0 ? "#808080" : d.color)
-      .attr("opacity", 0.5);
+    .enter()
+    .append("line")
+    .attr("stroke-width", d =>
+      d.stems < 0 ? 2 : Math.log(d.stems / 100, 2) * 3 + 4
+    )
+    .attr("stroke", d => (d.stems < 0 ? "#808080" : d.color))
+    .attr("opacity", 0.5);
 
-  const nodeElements = svgContainer.append("g")
+  const nodeElements = svgContainer
+    .append("g")
     .selectAll("circle")
     .data(nodes)
-    .enter().append("circle")
-      .attr("r", 30)
-      .attr("fill", d => d.color)
-      .call(dragDrop);
+    .enter()
+    .append("circle")
+    .attr("r", 30)
+    .attr("fill", d => d.color)
+    .call(dragDrop);
 
-  const textElements = svgContainer.append("g")
+  const textElements = svgContainer
+    .append("g")
     .selectAll("text")
     .data(nodes)
-    .enter().append("text")
-      .text(node => node.label)
-      .attr("font-size", 15)
-      .attr("dx", node => calculateDX(node.label))
-      .attr("dy", 5)
-      .style("pointer-events", "none");
+    .enter()
+    .append("text")
+    .text(node => node.label)
+    .attr("font-size", 15)
+    .attr("dx", node => calculateDX(node.label))
+    .attr("dy", 5)
+    .style("pointer-events", "none");
 
   simulation.nodes(nodes).on("tick", () => {
     linkElements
@@ -331,34 +357,34 @@ function displayModal(d, pairs) {
       .attr("y1", link => link.source.y)
       .attr("x2", link => link.target.x)
       .attr("y2", link => link.target.y);
-    nodeElements
-      .attr("cx", node => node.x)
-      .attr("cy", node => node.y)
-    textElements
-      .attr("x", node => node.x)
-      .attr("y", node => node.y)
+    nodeElements.attr("cx", node => node.x).attr("cy", node => node.y);
+    textElements.attr("x", node => node.x).attr("y", node => node.y);
   });
 
   simulation.force("link").links(connections);
 
   const stages = ["INCUBATOR", "NURSERY", "STAGING", "TRUNK"];
-  let legend = svgContainer.selectAll(".legend")
+  let legend = svgContainer
+    .selectAll(".legend")
     .data(stages)
-    .enter().append("g")
-    .attr("transform", function (d, i) {
+    .enter()
+    .append("g")
+    .attr("transform", function(d, i) {
       {
-          return "translate(0," + (i * 20 + 10) + ")"
+        return "translate(0," + (i * 20 + 10) + ")";
       }
     });
-    
-  legend.append("rect")
+
+  legend
+    .append("rect")
     .attr("x", 0)
     .attr("y", 0)
     .attr("width", 10)
     .attr("height", 10)
     .style("fill", stage => chooseNodeColor(stage.toLowerCase()));
 
-  legend.append("text")
+  legend
+    .append("text")
     .attr("x", 20)
     .attr("y", 10)
     .text(stage => stage)
